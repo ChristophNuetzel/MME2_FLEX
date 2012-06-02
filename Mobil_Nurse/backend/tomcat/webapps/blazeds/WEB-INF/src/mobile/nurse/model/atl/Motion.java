43263cd85d041c8b415ebf117ffbbd08d7f4b2b8
ChristophNuetzel <christoph_nuetzel@web.de>
2012-06-02 16:03:34 +0200

    Ordnername geaendert auf Ausgabe2

---
+++ Mobil_Nurse/backend/tomcat/webapps/blazeds/WEB-INF/src/mobile/nurse/model/atl/Motion.java
@@ -0,0 +1,14 @@
+package mobile.nurse.model.atl;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Motion {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+
+}
